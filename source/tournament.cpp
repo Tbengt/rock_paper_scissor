1df1e7ae2348aa3107ce3df20ca18c15c60242a3
Torbjorn Wasterlid <wasterlid.torbjorn@gmail.com>
2019-03-03 19:34:58 +0100

    Refactored tournament run method into several smaller methods and types

--- source/tournament.cpp
+++ source/tournament.cpp
@@ -2,36 +2,48 @@
 #include <map>
 #include <algorithm>
 
+using ScoreCard = std::map<Player, int>;
+
+static void playGameAndRegisterScore(const Player& player1, const Player& player2, ScoreCard& scoreCard);
+static Player findWinner(const ScoreCard& scoreCard);
+
 void Tournament::addPlayer(Player participant) {
         participants.push_back(participant);
 }
 
 Player Tournament::run() {
 
-        std::map<std::string, int> scoreCard;
+        ScoreCard scoreCard;
         for (auto& player : participants) {
-                scoreCard.insert(std::make_pair(player.name, 0));
+                scoreCard.insert(std::make_pair(player, 0));
         }
         
         for (auto& player1 : participants) {
                 for (auto& player2 : participants) {
-                        Winner winner = RockPaperScissorGame::play(std::bind( &Player::nextMove, player1),
-                                                                   std::bind( &Player::nextMove, player2));
-
-                        if (winner == Winner::player1)
-                                scoreCard[player1.name]++;
-                        else if (winner == Winner::player2)
-                                scoreCard[player2.name]++;
+                        playGameAndRegisterScore(player1, player2, scoreCard);
                 }
                 
         }
-
                         
-        auto winner = std::max_element(scoreCard.begin(), scoreCard.end(),
-                                       [](const std::pair<std::string, int>& p1, const std::pair<std::string, int>& p2) {
-                                               return p1.second < p2.second; });
-        return {(*winner).first};
-
+        return findWinner(scoreCard);
 }
 
 
+static void playGameAndRegisterScore(const Player& player1, const Player& player2, ScoreCard& scoreCard) {
+        Winner winner = RockPaperScissorGame::play(std::bind( &Player::nextMove, player1),
+                                                   std::bind( &Player::nextMove, player2));
+
+        if (winner == Winner::player1) {
+                scoreCard[player1]++;
+        }
+        else if (winner == Winner::player2) {
+                scoreCard[player2]++;
+        }
+}
+
+static Player findWinner(const ScoreCard& scoreCard) {
+        auto winner = std::max_element(scoreCard.begin(), scoreCard.end(),
+                                       [](const std::pair<Player, int>& p1, const std::pair<Player, int>& p2) {
+                                               return p1.second < p2.second; });
+        return (*winner).first;
+}
